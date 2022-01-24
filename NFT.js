dd0856dc91a6bc660d5e5599acbaaab34ced67ca
Lahiru Senavirathna <87580847+SE-LAPS@users.noreply.github.com>
2022-01-24 16:32:29 +0530

    NFT site
    New NFT site

---
+++ NFT.js
@@ -0,0 +1,80 @@
+let menu = document.querySelector('.menu-icon');
+
+let navbar = document.querySelector('.menu');
+
+menu.onclick = () => {
+
+    navbar.classList.toggle('active');
+    menu.classList.toggle('move');
+    bell.classList.remove('active');
+
+}
+
+let bell = document.querySelector('.notification');
+document.querySelector('#bell-icon').onclick = () => {
+
+    bell.classList.toggle('active');
+
+}
+
+var swiper = new Swiper(".trending-content", {
+
+    loop: true,
+    grabCursor: true,
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+
+        el: ".swiper-pagination",
+
+        clickable: true,
+
+    },
+
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+
+    },
+
+    breakpoints: {
+
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+
+        },
+
+        768: {
+
+            slidesPerView: 3,
+            spaceBetween: 15,
+
+        },
+
+        1068: {
+
+            slidesPerView: 5,
+            spaceBetween: 50,
+
+        },
+
+    },
+
+});
+
+
+// custom scroll bar
+
+
+
+window.onscroll = function() { mufunction() };
+
+function mufunction() {
+
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById('scroll-bar').style.width = scrolled + '%';
+
+}
